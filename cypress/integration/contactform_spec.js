b3a0f961f6f3ba1ba340be0de9ec58abaf2ec9a8
shwetaneelsharma <shwetasharma@axelerant.com>
2020-11-29 22:51:48 +0530

    Changes to contact form spec.

--- cypress/integration/contactform_spec.js
+++ cypress/integration/contactform_spec.js
@@ -8,15 +8,4 @@ describe('Contact us form', ()=> {
         cy.get('#edit-actions-submit').click();
         cy.percySnapshot('Contact form submission', { widths: [320, 1024, 1400] });
     });
-
-    it('should fail after CSS changes', function () {
-        cy.visit('/contact-us');
-        cy.get('#edit-name').type('Shweta Sharma');
-        cy.get('#edit-email').type('shwetasharma@axelerant.com');
-        cy.get('#edit-message').type('This is a test message!');
-        cy.get('#block-urbanharvestmap').invoke('hide');
-        cy.percySnapshot('Contact us form', { widths: [320, 1024, 1400] });
-        cy.get('#edit-actions-submit').click();
-        cy.percySnapshot('Contact form submission', { widths: [320, 1024, 1400] });
-    });
 })
